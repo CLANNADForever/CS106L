a64fb0f9d2c090b638bb8cd9bae687551a19ef44
Wangjishi <2054606820@qq.com>
2025-12-01 18:06:16 +0800

    完成hw4第一部分

--- assignment4/spellcheck.cpp
+++ assignment4/spellcheck.cpp
@@ -11,8 +11,26 @@ template <typename Iterator, typename UnaryPred>
 std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
-  /* TODO: Implement this method */
-  return Corpus();
+  // 1.获取一个所有指向空白字符的迭代器构成的列表
+  auto spaceIter = find_all(source.begin(), source.end(), ::isspace); // FIXME
+  
+  // 2.构建一个初始化Token的函数
+  auto buildToken = [&source] (auto it1, auto it2) {
+    return Token(source, it1, it2);
+  };
+
+  // 3.对每对相邻空白字符迭代器之间的序列，调用Token的初始化函数
+  Corpus tokens;
+  auto inserter = std::inserter(tokens, tokens.end());
+  std::transform(spaceIter.begin(), spaceIter.end() - 1, spaceIter.begin() + 1, inserter, buildToken);
+
+  // 4.从std::set<Token>中删除空标记
+  auto checkEmptyToken = [] (Token t) {
+    return t.content.empty();
+  };
+  std::erase_if(tokens, checkEmptyToken);
+
+  return tokens;
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
